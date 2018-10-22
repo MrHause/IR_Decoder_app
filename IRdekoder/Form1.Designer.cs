a405a4ef933efd2fc782b0f32afa8af91c5aa1c4
MrHause <mrhause@wp.pl>
2018-10-23 00:47:40 +0200

    First Commit

---
+++ IRdekoder/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace IRdekoder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.portlist = new System.Windows.Forms.ComboBox();
+            this.baudlist = new System.Windows.Forms.ComboBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.buttonSaveTime = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.buttonPlot = new System.Windows.Forms.Button();
+            this.buttonSaveChart = new System.Windows.Forms.Button();
+            this.buttonClearPlot = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // portlist
+            // 
+            this.portlist.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.portlist.FormattingEnabled = true;
+            this.portlist.Location = new System.Drawing.Point(26, 34);
+            this.portlist.Name = "portlist";
+            this.portlist.Size = new System.Drawing.Size(121, 21);
+            this.portlist.TabIndex = 0;
+            // 
+            // baudlist
+            // 
+            this.baudlist.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.baudlist.FormattingEnabled = true;
+            this.baudlist.Items.AddRange(new object[] {
+            "4800",
+            "9600",
+            "14400",
+            "19200",
+            "38400",
+            "57600"});
+            this.baudlist.Location = new System.Drawing.Point(153, 34);
+            this.baudlist.Name = "baudlist";
+            this.baudlist.Size = new System.Drawing.Size(121, 21);
+            this.baudlist.TabIndex = 1;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(292, 31);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(98, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonClear);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(26, 396);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(151, 277);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Receive";
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Enabled = false;
+            this.buttonClear.Location = new System.Drawing.Point(6, 248);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(138, 23);
+            this.buttonClear.TabIndex = 1;
+            this.buttonClear.Text = "Clear All";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(6, 19);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(138, 223);
+            this.textBox1.TabIndex = 0;
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(26, 61);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 4;
+            this.buttonOpen.Text = "Open Port";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Enabled = false;
+            this.buttonClose.Location = new System.Drawing.Point(107, 61);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(75, 23);
+            this.buttonClose.TabIndex = 5;
+            this.buttonClose.Text = "Close Port";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // buttonSaveTime
+            // 
+            this.buttonSaveTime.Enabled = false;
+            this.buttonSaveTime.Location = new System.Drawing.Point(183, 404);
+            this.buttonSaveTime.Name = "buttonSaveTime";
+            this.buttonSaveTime.Size = new System.Drawing.Size(75, 41);
+            this.buttonSaveTime.TabIndex = 6;
+            this.buttonSaveTime.Text = "Save times";
+            this.buttonSaveTime.UseVisualStyleBackColor = true;
+            this.buttonSaveTime.Click += new System.EventHandler(this.buttonSaveTime_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox2.ForeColor = System.Drawing.Color.Red;
+            this.textBox2.Location = new System.Drawing.Point(396, 30);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(292, 24);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Port name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(153, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Baud rate";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(289, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Status";
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(26, 90);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(1585, 300);
+            this.chart1.TabIndex = 11;
+            this.chart1.Text = "chart1";
+            // 
+            // buttonPlot
+            // 
+            this.buttonPlot.Enabled = false;
+            this.buttonPlot.Location = new System.Drawing.Point(183, 498);
+            this.buttonPlot.Name = "buttonPlot";
+            this.buttonPlot.Size = new System.Drawing.Size(75, 41);
+            this.buttonPlot.TabIndex = 12;
+            this.buttonPlot.Text = "Plot";
+            this.buttonPlot.UseVisualStyleBackColor = true;
+            this.buttonPlot.Click += new System.EventHandler(this.buttonPlot_Click);
+            // 
+            // buttonSaveChart
+            // 
+            this.buttonSaveChart.Enabled = false;
+            this.buttonSaveChart.Location = new System.Drawing.Point(183, 451);
+            this.buttonSaveChart.Name = "buttonSaveChart";
+            this.buttonSaveChart.Size = new System.Drawing.Size(75, 41);
+            this.buttonSaveChart.TabIndex = 13;
+            this.buttonSaveChart.Text = "Save Chart";
+            this.buttonSaveChart.UseVisualStyleBackColor = true;
+            this.buttonSaveChart.Click += new System.EventHandler(this.buttonSaveChart_Click);
+            // 
+            // buttonClearPlot
+            // 
+            this.buttonClearPlot.Enabled = false;
+            this.buttonClearPlot.Location = new System.Drawing.Point(183, 545);
+            this.buttonClearPlot.Name = "buttonClearPlot";
+            this.buttonClearPlot.Size = new System.Drawing.Size(75, 41);
+            this.buttonClearPlot.TabIndex = 14;
+            this.buttonClearPlot.Text = "Clear Plot";
+            this.buttonClearPlot.UseVisualStyleBackColor = true;
+            this.buttonClearPlot.Click += new System.EventHandler(this.buttonClearPlot_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1623, 685);
+            this.Controls.Add(this.buttonClearPlot);
+            this.Controls.Add(this.buttonSaveChart);
+            this.Controls.Add(this.buttonPlot);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.buttonSaveTime);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.baudlist);
+            this.Controls.Add(this.portlist);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IR dekoder";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox portlist;
+        private System.Windows.Forms.ComboBox baudlist;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Button buttonSaveTime;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button buttonPlot;
+        private System.Windows.Forms.Button buttonSaveChart;
+        private System.Windows.Forms.Button buttonClearPlot;
+    }
+}
+
---
+++ IRdekoder/Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Windows.Forms.DataVisualization.Charting;
+
+
+namespace IRdekoder
+{
+    public partial class Form1 : Form
+    {
+        private string in_data;
+        private string in_data2;
+        int color_index;
+        List<int> int_list = new List<int>();
+        List<int> time_list = new List<int>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            getAvailablePorts();
+            InitializePlot();
+        }
+        void InitializePlot()
+        {
+
+            chart1.Series.Add("Signal");
+            chart1.Series.Add("Signal2");
+
+            chart1.Series["Signal"].ChartType = SeriesChartType.Line;
+            chart1.Series["Signal2"].ChartType = SeriesChartType.Line;
+
+            chart1.ChartAreas[0].AxisX.MajorGrid.LineWidth = 0;
+            chart1.ChartAreas[0].AxisY.MajorGrid.LineWidth = 0;
+
+            chart1.Series[0].IsVisibleInLegend = false;
+
+        }
+
+        void getAvailablePorts()
+        {
+            string[] ports = SerialPort.GetPortNames();
+            this.portlist.Items.AddRange(ports);
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (portlist.Text == "" || baudlist.Text == "")
+                {
+                    textBox2.Visible = true;
+                    textBox2.Text = "Select the connection properties";
+                }
+                else
+                {
+                    serialPort1.PortName = portlist.Text;
+                    serialPort1.BaudRate = Convert.ToInt32(baudlist.Text);
+                    serialPort1.Parity = Parity.None;
+                    serialPort1.StopBits = StopBits.One;
+                    serialPort1.DataBits = 8;
+                    serialPort1.Handshake = Handshake.None;
+                    serialPort1.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+                    serialPort1.Open();
+                    progressBar1.Value = 100;
+                    buttonOpen.Enabled = false;
+                    buttonClose.Enabled = true;
+                    buttonClear.Enabled = true;
+                    buttonPlot.Enabled = true;
+                    buttonSaveTime.Enabled = true;
+                    buttonSaveChart.Enabled = true;
+                    buttonClearPlot.Enabled = true;
+                    textBox1.Enabled = true;
+                    textBox2.Visible = true;
+                    textBox2.Text = "Connected";
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                textBox2.Visible = true;
+                textBox2.Text = "Unauthorized Access";
+            }
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            progressBar1.Value = 0;
+            buttonClose.Enabled = false;
+            buttonOpen.Enabled = true;
+            buttonClear.Enabled = false;
+            buttonSaveTime.Enabled = false;
+            textBox1.Enabled = false;
+            textBox2.Clear();
+            textBox2.Visible = false;
+            buttonPlot.Enabled = false;
+            buttonSaveChart.Enabled = false;
+            buttonClearPlot.Enabled = false;
+        }
+
+        void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            in_data2 = serialPort1.ReadExisting();
+            in_data += in_data2;
+            this.Invoke(new EventHandler(display_event));
+        }
+
+        private void display_event(object sender, EventArgs e)
+        {
+            textBox1.AppendText(in_data2);
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+            in_data = "";
+
+            time_list.Clear();
+            int_list.Clear();
+            foreach (var series in chart1.Series)
+            {
+                series.Points.Clear();
+            }
+            color_index = 0;
+        }
+
+        private void buttonSaveTime_Click(object sender, EventArgs e)
+        {
+
+            chart1.Series["Signal"].Color = Color.Red;
+
+            FileDialog oDialog = new SaveFileDialog();
+            oDialog.DefaultExt = "txt";
+            oDialog.FileName = "times";
+
+            List<string> output = new List<string>(
+               in_data.Split(new string[] { "\r\n" },
+               StringSplitOptions.RemoveEmptyEntries));
+
+            output.RemoveAt(0);
+
+            if (oDialog.ShowDialog() == DialogResult.OK)
+            {
+
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(@oDialog.FileName))
+                    foreach (string line in output)
+                    {
+                        file.WriteLine(line);
+                    }
+            }
+        }
+
+        private void buttonPlot_Click(object sender, EventArgs e)
+        {
+
+            List<string> list = new List<string>(
+                in_data.Split(new string[] { "\r\n" },
+                StringSplitOptions.RemoveEmptyEntries));
+            int time = 0;
+            int x = 0;
+            foreach (string el in list)
+            {
+                Int32.TryParse(el, out x);
+                int_list.Add(x);
+            }
+
+            for (int i = 1; i < int_list.Count; i++)
+            {
+                time += int_list[i];
+                time_list.Add(time);
+            }
+            MakeChart(time_list);
+            //clear saved data
+            in_data = "";
+            time_list.Clear();
+            int_list.Clear();
+        }
+        private void MakeChart(List<int>time_list)
+        {
+            
+            var chart = chart1.ChartAreas[0];
+            chart.AxisX.IntervalType = DateTimeIntervalType.Number;
+
+            chart.AxisX.LabelStyle.Format = "";
+            chart.AxisY.LabelStyle.Format = "";
+            chart.AxisY.LabelStyle.IsEndLabelVisible = true;
+
+            chart.AxisX.Minimum = time_list[0] - 500;
+            chart.AxisX.Maximum = time_list[time_list.Count - 1] + 100;
+
+            chart.AxisY.Maximum = 2;
+            chart.AxisY.Minimum = 0;
+
+            chart.AxisX.Interval = 2000;
+            chart.AxisY.Interval = 1;
+            
+            if(color_index==0)
+            {
+                chart1.Series["Signal"].Color = Color.Red;
+                int temp = 0;
+                time_list.RemoveAt(time_list.Count - 1);
+                //adding points
+                chart1.Series["Signal"].Points.AddXY(time_list[0] - 500, 0);
+
+                foreach (int el1 in time_list)
+                {
+                    if (temp % 2 == 0)
+                    {
+                        chart1.Series["Signal"].Points.AddXY(el1, 0);
+                        chart1.Series["Signal"].Points.AddXY(el1, 1);
+                    }
+
+                    else
+                    {
+                        chart1.Series["Signal"].Points.AddXY(el1, 1);
+                        chart1.Series["Signal"].Points.AddXY(el1, 0);
+                    }
+                    temp++;
+                }
+            }
+            else if(color_index==1)
+            {
+                
+                
+                chart1.Series["Signal2"].Color = Color.Blue;
+                int temp = 0;
+                time_list.RemoveAt(time_list.Count - 1);
+                //adding points
+
+                chart1.Series["Signal2"].Points.AddXY(time_list[0] - 500, 0);
+
+                foreach (int el1 in time_list)
+                {
+                    if (temp % 2 == 0)
+                    {
+                        chart1.Series["Signal2"].Points.AddXY(el1, 0);
+                        chart1.Series["Signal2"].Points.AddXY(el1, 1);
+                    }
+
+                    else
+                    {
+                        chart1.Series["Signal2"].Points.AddXY(el1, 1);
+                        chart1.Series["Signal2"].Points.AddXY(el1, 0);
+                    }
+                    temp++;
+                }
+            }
+            color_index++;
+        }
+
+        private void buttonSaveChart_Click(object sender, EventArgs e)
+        {
+            FileDialog oDialog = new SaveFileDialog();
+            oDialog.FileName = "chart";
+            oDialog.Title = "Save Chart As Image File";
+            oDialog.DefaultExt = "png";
+            oDialog.Filter = "PNG Image|*.png|JPeg Image|*.jpg|BMP Image|*.bmp";
+
+            if (oDialog.ShowDialog() == DialogResult.OK)
+            {
+                if (oDialog.FilterIndex == 3)
+                    chart1.SaveImage(oDialog.FileName, ChartImageFormat.Bmp);
+                if (oDialog.FilterIndex == 2)
+                    chart1.SaveImage(oDialog.FileName, ChartImageFormat.Jpeg);
+                if (oDialog.FilterIndex == 1)
+                    chart1.SaveImage(oDialog.FileName, ChartImageFormat.Png);
+            }
+        }
+
+        private void buttonClearPlot_Click(object sender, EventArgs e)
+        {
+            in_data = "";
+
+            time_list.Clear();
+            int_list.Clear();
+            foreach (var series in chart1.Series)
+            {
+                series.Points.Clear();
+            }
+            color_index = 0;
+        }
+    }
+}
